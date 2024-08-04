46c613519e7e7f2e1e522d3997be0ed92775425e
kariyayo <mail.bati11@gmail.com>
2024-08-04 14:39:43 +0900

    add "attr_color" in sample2

--- feature/myopengles/src/main/cpp/gl/sample2.c
+++ feature/myopengles/src/main/cpp/gl/sample2.c
@@ -2,6 +2,7 @@
 
 typedef struct {
     GLint attr_pos;
+    GLubyte attr_color;
     GLint unif_move_pos;
     vec2 pos;
 } Extension_Sample2;
@@ -15,18 +16,22 @@ GLApp* sample2_initialize() {
 
     // 頂点シェーダー
     // このサンプルではrendering時に、2次元の頂点を3つ与えられるので、3回呼ばれる（GPUによって並列で実行される）
-    const GLchar *vertex_shader_source = "attribute mediump vec4 attr_pos;" // 中精度のvec4型、変数名はattr_pos
-                                         "uniform mediump vec2 unif_move_pos;"
+    const GLchar *vertex_shader_source = "attribute mediump vec4 attr_pos;"    // 中精度のvec4型、変数名はattr_pos
+                                         "attribute lowp vec4 attr_color;"     // 頂点カラー
+                                         "varying lowp vec4 vary_color;"       // "varying"で頂点シェーダーからフラグメントシェーダーへ値を渡す（この時値が変化する）
+                                         "uniform mediump vec2 unif_move_pos;" // 頂点位置を動かす
                                          "void main() {"
                                          "  gl_Position = attr_pos;"
                                          "  gl_Position.xy += unif_move_pos;"
+                                         "  vary_color = attr_color;" // 頂点カラー
                                          "}";
 
     // フラグメントシェーダー（ピクセルシェーダーとも呼ばれる）
     // 塗りつぶされるピクセルの数だけ呼び出される
     // FHDサイズのディスプレイだと 1920 * 1080 = 124,416,000回呼ばれる（GPUによって並列で実行される）
-    const GLchar *fragment_shader_source = "void main() {"
-                                           "    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);"
+    const GLchar *fragment_shader_source = "varying lowp vec4 vary_color;"
+                                           "void main() {"
+                                           "    gl_FragColor = vary_color;"
                                            "}";
 
     // コンパイルとリンクを行う
@@ -34,9 +39,13 @@ GLApp* sample2_initialize() {
             vertex_shader_source,
             fragment_shader_source);
 
+    // attribute変数(頂点ごとの情報)
     ext->attr_pos = glGetAttribLocation(app->shader_program, "attr_pos");
     assert(ext->attr_pos >= 0);
+    ext->attr_color = glGetAttribLocation(app->shader_program, "attr_color");
+    assert(ext->attr_color >= 0);
 
+    // uniform変数(アプリレベルの情報)
     ext->unif_move_pos = glGetUniformLocation(app->shader_program, "unif_move_pos");
     assert(ext->unif_move_pos >= 0);
 
@@ -64,14 +73,24 @@ void sample2_rendering(GLApp* app, int width, int height) {
 
     Extension_Sample2 *ext = (Extension_Sample2*)app->ext;
 
+    // attribute変数を有効にする
     glEnableVertexAttribArray(ext->attr_pos);
+    glEnableVertexAttribArray(ext->attr_color);
+
     const GLfloat position[] = {
             0.0f, 0.5f,
             0.0f, 0.0f,
             0.5f, 0.5f,
             0.5f, 0.0f,
     };
+    const GLubyte color[] = {
+            255, 0, 0,
+            0, 255, 0,
+            0, 0, 255,
+            255, 255, 255,
+    };
     glVertexAttribPointer(ext->attr_pos, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*)position);
+    glVertexAttribPointer(ext->attr_color, 3, GL_UNSIGNED_BYTE, GL_TRUE, 0, color);
 
     {
         ext->pos.x += 0.01f;
@@ -89,6 +108,7 @@ void sample2_rendering(GLApp* app, int width, int height) {
 
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
+    // バックバッファをフロントバッファへ転送（プラットフォーム依存）
     postFrontBuffer(app);
 }
 
